dada1d763c64f648fee77aa7787a1928e46136be
Matheus Cristofoli <matfrietasc@gmail.com>
2022-05-13 04:46:41 -0400

    add if response is forbiden return to login

--- client/src/Pages/profile/Profile.jsx
+++ client/src/Pages/profile/Profile.jsx
@@ -6,6 +6,7 @@ import useAuth from '../../hooks/useAuth';
 import UserPost from '../../Components/layout/userposts/Post'; // The layout element for the user's posts
 
 export default function Profile() {
+  const navigate = useNavigate();
   const axiosPrivate = useAxiosPrivate();
   const { auth } = useAuth();
   const [posts, setPosts] = useState([]);
@@ -27,6 +28,9 @@ export default function Profile() {
         })
         .catch((err) => {
           console.log(err);
+          if (err.response.status === 401) {
+            navigate('/login');
+          }
         });
     }
   }, [userId]);
@@ -34,51 +38,53 @@ export default function Profile() {
   return (
     <>
       <Navbar />
-      <div className='flex justify-center mx-auto mt-2 h-10  '>
-        <div className='absolute z-[-10]  max-w-xl  '>
-          <img
-            src='https://png.pngtree.com/thumb_back/fh260/background/20200714/pngtree-modern-double-color-futuristic-neon-background-image_351866.jpg'
-            alt='Profile banner'
-            className='object-cover w-full h-full rounded-lg'
-          />
-        </div>
-      </div>
-      <div className='max-w-xl px-8 py-4 mx-auto mt-[10rem] bg-white rounded-lg shadow-lg dark:bg-gray-100 '>
-        <div className='flex justify-center -mt-16 md:justify-start'></div>
-        <div className=''>
-          <img
-            className='object-cover w-20 h-20 border-2 border-logoOrange rounded-full  z-10dark:logoOrange'
-            alt='Testimonial avatar'
-            src='https://images.unsplash.com/photo-1499714608240-22fc6ad53fb2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=76&q=80'
-          />
-          <h2 className='mt-2 text-2xl font-semibold text-gray-800 dark:text-gray-600 md:mt-0 md:text-3xl z-10'>
-            {auth?.username}
-          </h2>
-
-          <p className='mt-2 text-gray-600 dark:text-gray-200 z'>{user.bio}</p>
-
-          <div className='flex justify-end mt-4'>
-            <p className='text-xl font-medium text-blue-500 dark:text-blue-300'>
-              {auth?.name}
-            </p>
+      <div className='container max-w-xl '>
+        <div className=' '>
+          <div className='absolute z-[-10] '>
+            <img
+              src='https://png.pngtree.com/thumb_back/fh260/background/20200714/pngtree-modern-double-color-futuristic-neon-background-image_351866.jpg'
+              alt='Profile banner'
+              className='object-cover w-full h-full rounded-lg'
+            />
           </div>
         </div>
-      </div>
-      <div className='max-w-lg pt-1 mx-auto mt-2 bg-white rounded-lg shadow-lg dark:bg-gray-600 '>
-        {/* Not a function? */}
-        {console.log(posts)}
-        {posts.length !== 0 &&
-          posts.map((post) => (
-            <UserPost
-              key={post.id}
-              userId={post.userId}
-              postId={post.postId}
-              username={post.username}
-              imageUrl={post.imageUrl}
-              content={post.content}
-              postTitle={post.postTitle}
+        <div className='px-8 py-4 mx-auto mt-[10rem] bg-white rounded-lg shadow-lg dark:bg-gray-100 '>
+          <div className='flex justify-center -mt-16 md:justify-start'></div>
+          <div className=''>
+            <img
+              className='object-cover w-20 h-20 border-2 border-logoOrange rounded-full  z-10dark:logoOrange'
+              alt='Testimonial avatar'
+              src='https://images.unsplash.com/photo-1499714608240-22fc6ad53fb2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=76&q=80'
             />
-          ))}
+            <h2 className='mt-2 text-2xl font-semibold text-gray-800 dark:text-gray-600 md:mt-0 md:text-3xl z-10'>
+              {auth?.username}
+            </h2>
+
+            <p className='mt-2 text-gray-600 dark:text-gray-200 z'>
+              {user.bio}
+            </p>
+
+            <div className='flex justify-end mt-4'>
+              <p className='text-xl font-medium text-blue-500 dark:text-blue-300'>
+                {auth?.name}
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className='pt-1 mx-auto mt-2 bg-white rounded-lg shadow-lg dark:bg-gray-600 '>
+          {posts.length !== 0 &&
+            posts.map((post) => (
+              <UserPost
+                key={post.id}
+                userId={post.userId}
+                postId={post.postId}
+                username={post.username}
+                imageUrl={post.imageUrl}
+                content={post.content}
+                postTitle={post.postTitle}
+              />
+            ))}
+        </div>
       </div>
     </>
   );
