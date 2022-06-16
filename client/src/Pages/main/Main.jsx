026e53f360997f3fa5c7a263e8b88207b502a77e
Matheus Cristofoli <matfrietasc@gmail.com>
2022-06-16 06:14:56 -0400

    Custom margin applied to the main page

--- client/src/Pages/main/Main.jsx
+++ client/src/Pages/main/Main.jsx
@@ -6,7 +6,7 @@ function Main() {
   return (
     <div className=' overflow-x-hidden'>
       <Navbar />
-      <div className='flex flex-col m-auto max-w-4xl'>
+      <div className='flex flex-col m-auto max-w-4xl  mt-2 transition-all md:mt-10 '>
         <Posts />
       </div>
     </div>
